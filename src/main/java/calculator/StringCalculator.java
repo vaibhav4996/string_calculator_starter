49b714f38da4dca52e50d52bb597a6d4864435cc
Vaibhav Awagan <Vaibhavawagan1@gmail.com>
2021-10-17 22:26:52 +0530

    delimiter

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,36 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
-    }
+	public int calculate(String text) {
+        List<String> negatives = new ArrayList<String>();
 
-}
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+        char customDelimiter = getCustomDelimiter(text.split("\n")[0]);
+
+        String[] chuncks = (customDelimiter == ',') ? 
+                                text.split(",|\n") : text.split("\n|" + customDelimiter);
+        int total = 0;
+        int skipFirstLines = (customDelimiter == ',') ? 0 : 2;
+        for (String item : chuncks) {
+            if (skipFirstLines > 0) {
+                --skipFirstLines;
+            } else {
+                int num = Integer.parseInt(item);
+                if (num < 0) {
+                    negatives.add(item);
+                }
+                total += num;
+            }
+        }
+	
+
+	private char getCustomDelimiter(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
